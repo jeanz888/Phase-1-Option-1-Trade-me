d74f3bcac6458282a00427505185afe86b171147
jeanz888 <157359660+jeanz888@users.noreply.github.com>
2024-09-05 15:05:22 +1200

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { createTheme } from '@material-ui/core/styles';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ItemDetails from './pages/ItemDetails';
-import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createTheme } from '@material-ui/core/styles'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ItemDetails from './pages/ItemDetails'
+import { AuthProvider } from './contexts/AuthContext'
 
 const theme = createTheme();
 
